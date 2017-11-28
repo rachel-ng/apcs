e1bbe3a9c266e34efa4e4bd83f7664bd716a4374
rachel-ng <rng@stuy.edu>
2017-11-28 15:29:20 -0500

    added stuff

--- 05Library/Book.java
+++ 05Library/Book.java
@@ -1,7 +1,44 @@
 public class Book {
     private String author;
-    
-    
-    public Book {
+    private String title;
+    private String ISBN;
+
+    public null Book () {
 	
     }
+    
+    public Book (String auth, String titl, String bookNum) {
+	title = titl;
+	author = auth;
+	ISBN = bookNum;
+    }
+
+    public String getAuthor () {
+	return author;
+    }
+
+    public String getTitle () {
+	return title;
+    }
+
+    public String getISBN () {
+	return ISBN;
+    }
+    
+    public void setAuthor (String auth) {
+	author = auth;
+    }
+
+    public void setTitle (String titl) {
+	title = titl;
+    }
+
+    public void setISBN (String bookNum) {
+	ISBN = bookNum;
+    }
+
+    public String toString () {
+	return "Title: " + title + "\nAuthor: " + author + "\nISBN: " + ISBN; 
+    }
+    
+}
