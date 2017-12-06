885b89c186c5b6e125c8302e4a3cb7748a0ee5c4
rng <rng@stuy.edu>
2017-12-06 00:48:31 -0500

    the driver i tested with

--- 07Barcode/Driver.java
+++ 07Barcode/Driver.java
@@ -1,9 +1,11 @@
 public class Driver {
     public static void main(String[] args) {
+	Barcode meh = new Barcode ("11111");
 	Barcode halp = new Barcode("54321");
 	System.out.println(halp.getZip() + "\t" + halp.checkSum());
 	System.out.println(halp.getCode());
 	System.out.println(halp.toZip(halp.getCode()));
+	System.out.println(halp.equal(meh));
 	System.out.println(halp.toString());
     }
 }
