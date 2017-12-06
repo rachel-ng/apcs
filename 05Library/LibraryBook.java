32a4dcd6783e26c50f42c6bd2defc79d55432f0f
rng <rng@stuy.edu>
2017-12-06 02:47:50 -0500

    added LibraryBook.java

--- 05Library/LibraryBook.java
+++ 05Library/LibraryBook.java
@@ -1,57 +1,31 @@
-abstract Class LibraryBook extends Book implements Comparable<LibraryBook> {
+public abstract class LibraryBook extends Book implements Comparable<LibraryBook> {
     private String callNumber;
 
-    public LibraryBook (String titl, String auth, String bookNum, String callNum) {
-	title = titl;
-	author = auth;
-	ISBN = bookNum;
-	callNumber = callNum;
+    public LibraryBook (String au, String ti, String is, String cn) {
+	super(au, ti, is);
+	callNumber = cn;
     }
     
-    public String getAuthor () {
-	return author;
-    }
-
-    public String getTitle () {
-	return title;
-    }
-
-    public String getISBN () {
-	return ISBN;
-    }
-
     public String getCallNumber () {
 	return callNumber;
     }
     
-    public void setAuthor (String auth) {
-	author = auth;
-    }
-
-    public void setTitle (String titl) {
-	title = titl;
-    }
-
-    public void setISBN (String bookNum) {
-	ISBN = bookNum;
-    }
-
-    public void setCallNumber (String callNum) {
-	callNumber = callNum;
+    public void setCallNumber (String cn) {
+	callNumber = cn;
     }
 
     public String toString () {
-	return "Title: " + title + "\nAuthor: " + author + "\nISBN: " + ISBN + "\nCirculation Status: " + circulationStatus() + "\nCall Number: " + callNumber; 
+	return getTitle() + ", " + getAuthor() + ", " + getISBN() + ", " + circulationStatus() + ", " + getCallNumber();
     }
     
-    abstract void checkout(String patron, String due);
+    public abstract void checkout(String patron, String due);
     
-    abstract void returned();
+    public abstract void returned();
     
-    abstract String circulationStatus();
+    public abstract String circulationStatus();
 
     public int compareTo (LibraryBook other) {
-	return (getCallNumber() - other.getCallNumber());
+	return getCallNumber.compareTo(other.getCallNumber());
     }
     
 }
