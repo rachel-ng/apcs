4a9e7d55b03e1f578cfd0b4ed71b4042715874f9
rng <rng@stuy.edu>
2017-12-06 00:33:27 -0500

    it works hooray

--- 07Barcode/Barcode.java
+++ 07Barcode/Barcode.java
@@ -1,15 +1,25 @@
 public class Barcode{
-    private String _zip;
-    String[] code = new String[] {"||:::", ":::||", "::|:|", "::||:", ":|::|", ":|:|:", ":||::", "|:::|", "|::|:", "|:|::", "||:::"};
 
+    private String _zip;
+    private String barcode;
+
+    
     // How do I make emacs stop deleting my code every once in a while .-.
 
     public Barcode (String zip) {
+	 _zip = zip;
 	if (zip.length() != 5) {
 	    throw new IllegalArgumentException();
 	}
 	else {
-	    _zip = zip;
+	    try {
+		for (int i = 0; i < zip.length(); i++) {
+		    Integer.parseInt(zip.substring(i,i+1));
+		}
+	    }
+	    catch (NumberFormatException e) {
+		throw new IllegalArgumentException();
+	    }
 	}
     }
 
@@ -22,7 +32,7 @@ public class Barcode{
     }
 
     public String toString () {
-	return getCode() + " (" +  getZip() + checkSum() + ")"+ "\nEMACS STOP DELETING MY CODE ASDFGHJKL";
+	return getCode() + " (" +  getZip() + checkSum() + ")";
     }
 
     public int checkSum () {
@@ -33,32 +43,73 @@ public class Barcode{
 	}
 	return sum % 10;
     }
-
-    private String converter (int num) {
-	return code[num];
-    }
-
+    
     private static String toCode (String zip) {
+	String[] cypher = new String[] {"||:::", ":::||", "::|:|", "::||:", ":|::|", ":|:|:", ":||::", "|:::|", "|::|:", "|:|::", "||:::"};
 	String bar = "|";
-	int num = 0;
+	int checkSum = 0;
+		
 	if (zip.length() != 5) {
 	    throw new IllegalArgumentException();
 	}
-	
+
 	else {
-	    for (int i = 0; i < zip.length(); i++) {
-		bar += converter(Integer.parseInt(zip.substring(i,i+1)));
+	    try {
+		for (int i = 0; i < zip.length(); i++) {
+		    Integer.parseInt(zip.substring(i,i+1));
+		}
 	    }
-	    return bar + converter(checkSum()) +  "|";
+	    catch (NumberFormatException e) {
+		throw new IllegalArgumentException();
+	    }
 	}
+	
+	for (int i = 0; i < zip.length(); i++) {
+	    bar += cypher[Integer.parseInt(zip.substring(i,i+1))];
+	    checkSum += Integer.parseInt(zip.substring(i,i+1));
+	}
+	checkSum = checkSum % 10;
+	return bar + cypher[checkSum] + "|";
     }
 
     public String toZip (String code) {
-	if (code.length() != 32) {
+        String[] cypher = new String[] {"||:::", ":::||", "::|:|", "::||:", ":|::|", ":|:|:", ":||::", "|:::|", "|::|:", "|:|::", "||:::"};
+	String zip = "";
+	int checkSum = 0;
+	int num = 0;
+	
+	if (code.length() != 32 || !code.substring(0,1).equals("|") && !code.substring(31,32).equals("|")) {
 	    throw new IllegalArgumentException();
 	}
+	else {
+	    code = code.substring(1,31);
+	    
+	    for (int i = 0; i < code.length(); i++) {
+		if (!code.substring(i, i+1).equals("|") && !code.substring(i, i+1).equals(":")){
+		    throw new IllegalArgumentException();
+		}
+	    }
+
+	    for (int i = 0; i < code.length(); i += 5) {
+		for (int x = 0; x < 10; x++) {
+		    if (code.substring(i,i+5).equals(cypher[x])) {
+			zip += x;
+			checkSum += x;
+		    }
+		}
+	    }
+
+	    num =  Integer.parseInt(zip.substring(5,6));
+	    checkSum = (checkSum - num) % 10;
+
+	    if (checkSum != num) {
+		throw new IllegalArgumentException();
+	    }
+	}
+	return zip.substring(0,5);
     }
 
-    //    public int compareTo (Barcode other) {
-    //   }
+    //public int compareTo (Barcode other) {
+    //return this.getZip().compareTo(other.getZip());
+    //}
 }
--- 07Barcode/Driver.java
+++ 07Barcode/Driver.java
@@ -1,9 +1,10 @@
 public class Driver {
     public static void main(String[] args) {
-	Barcode halp = new Barcode("11111");
+	Barcode halp = new Barcode("54321");
 	System.out.println(halp.getZip());
 	System.out.println(halp.checkSum());
 	System.out.println(halp.getCode());
+	System.out.println(halp.toZip(halp.getCode()));
 	System.out.println(halp.toString());
     }
 }
