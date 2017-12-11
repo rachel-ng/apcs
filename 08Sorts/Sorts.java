d05a26fd772ab55e8404bd511eff1a37180d7b33
rng <rng@stuy.edu>
2017-12-11 01:35:16 -0500

    bubblesort has yet to work :/

--- 08Sorts/Sorts.java
+++ 08Sorts/Sorts.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Sorts{
     public static String name (){
-	return "10,Ng,Rachel";
+	return "10,Ng,Rachel"; 
     }
     
     public static boolean isSorted (int[]ary){
@@ -29,7 +29,21 @@ public class Sorts{
 	int c = ary[a];
 	ary[a] = ary[b];
         ary[b] = c;
-    }  
+    }
+
+    
+    public static void insertionSort (int[] data) {
+	int num = 0;
+	
+	while (num < data.length) {
+	    int i = num;
+	    while (i > 0 && data[i] < data[i-1]) {
+		swap(data,i,i-1);
+		i = i - 1;
+	    }
+	    num ++;
+	}
+    }
 
     public static void selectionSort (int[] data) {
 	int num = 0;
@@ -49,16 +63,13 @@ public class Sorts{
 	}
     }
 
-    public static void insertionSort (int[] data) {
-	int num = 0;
-	
-	while (num < data.length) {
-	    int i = num;
-	    while (i > 0 && data[i] < data[i-1]) {
-		swap(data,i,i-1);
-		i = i - 1;
+    public static void bubbleSort (int[] data) {
+	while (!isSorted(data)) {
+	    for (int i = 0; i < data.length; i++) {
+		if (data[i] > data[i]) {
+		    swap(data,i,i+1);
+		}
 	    }
-	    num ++;
 	}
     }
 
@@ -73,6 +84,11 @@ public class Sorts{
             randish1[i] =(int)(Math.random()*100);
         }
 
+	int[] randish2 = new int[5];
+        for(int i = 0 ; i < randish2.length; i++){
+            randish2[i] =(int)(Math.random()*100);
+        }
+
         System.out.println(Arrays.toString(randish));
         selectionSort(randish);
 	System.out.println(Arrays.toString(randish));
@@ -80,5 +96,9 @@ public class Sorts{
 	System.out.println(Arrays.toString(randish1));
         insertionSort(randish1);
 	System.out.println(Arrays.toString(randish1));
+
+	System.out.println(Arrays.toString(randish2));
+        bubbleSort(randish2);
+	System.out.println(Arrays.toString(randish2));
     }
 }
