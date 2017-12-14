6dc877b0d3a2b0cf36e3455cac16dd627ec4be9f
rng <rng@stuy.edu>
2017-12-14 01:57:20 -0500

    moved some files around

--- temp.java
+++
@@ -1,9 +0,0 @@
-public class Temperature {
-    public static double CtoF (double t) {
-	return (t * 1.8) + 32;
-    }
-
-    public static double FtoC (double t) {
-	return (t - 32) / 1.8;
-    }
-}
