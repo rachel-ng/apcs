a7b4dee2bd4be8af36eb4f38393375b234850571
rng <rng@stuy.edu>
2017-12-14 01:52:59 -0500

    deleted filedump

--- filesdump/Demo1.java
+++
@@ -1,8 +0,0 @@
-//for easy printing of arrays
-import java.util.Arrays;
-public class Demo1{
-  public static void main(String[]args){
-    //args has your terminal options
-    System.out.println(Arrays.toString(args));
-  }
-}
