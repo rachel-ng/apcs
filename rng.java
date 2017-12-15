432904bff8d86a9292a376b64e661a7e83202102
rng <rng@stuy.edu>
2017-12-14 21:02:51 -0500

    it me

--- rng.java
+++ rng.java
@@ -2,14 +2,6 @@ import java.util.Random;
 public class rng{
   public static void main(String[]args){
     Random rand = new Random();
-    //for(int i =  0; i < 5; i++ ){
     System.out.println(rand.nextInt() % 1000);
-    //}
-    /* System.out.println();
-    rng = new Random(100);//seed of 100 is stored.
-    for(int i =  0; i < 5; i++ ){
-      System.out.println(rng.nextInt() % 1000);
-    }
-    */
   }
 }
