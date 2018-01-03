34cf3be228c59d8f6e2b46a74148d41b2ecb3b16
rachel-ng <rng@stuy.edu>
2018-01-03 15:31:55 -0500

    asdfasdfasdf

--- finalprojectstuffs/mainDraw.java
+++ finalprojectstuffs/mainDraw.java
@@ -7,40 +7,40 @@ import java.awt.event.*;
 
 public class mainDraw extends JComponent {
 
-    public int x = 500;
-    public int y = 500;
+    public int x = 100;
+    public int y = 100;
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawRect(x, y, 50, 50);
-        g.fillRect(x, y, 50, 50);
+        g.drawRect(x, y, 5, 5);
+        g.fillRect(x, y, 5, 5);
         g.setColor(Color.BLACK);
     }
 
     public void moveRight() {
         if (x + 50 < 600) {
-	    x = x + 10;
+	    x = x + 20;
 	    repaint();
 	}
     }
 
     public void moveLeft() {
 	if (x > 0) {
-	    x = x - 10;
+	    x = x - 20;
 	    repaint();
 	}
     }
 
     public void moveDown() {
 	if (y + 50 < 600) {
-	    y = y + 10;
+	    y = y + 20;
 	    repaint();
 	}
     }
 
     public void moveUp() {
 	if (y > 0) {
-	    y = y - 10;
+	    y = y - 20;
 	    repaint();
 	}
     }
--- finalprojectstuffs/mainFrame.java
+++ finalprojectstuffs/mainFrame.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class mainFrame extends JFrame implements MouseListener,MouseWheelListener{
+public class mainFrame extends JFrame implements MouseListener,MouseWheelListener,MouseMotionListener{
     private mainDraw draw;
 
     public void mouseEntered(MouseEvent e) {
@@ -16,9 +16,33 @@ public class mainFrame extends JFrame implements MouseListener,MouseWheelListene
 	System.out.println("mouseExited");
     }
     
-    public void mousePressed(MouseEvent e) {
-        System.out.println("mousePressed");
+    public void mouseMoved(MouseEvent e) {
+	System.out.println("mouseMoved");
+    }
+    
+    public void mouseDragged(MouseEvent e) {
+	System.out.println("mouseDragged");
 
+	if (e.getX() >= draw.x && e.getY() <= draw.y) {
+	    draw.moveRight();
+	    draw.moveUp();
+	}
+	else if (e.getX() >= draw.x && e.getY() > draw.y) {
+	    draw.moveRight();
+	    draw.moveDown();
+	}
+	else if (e.getX() < draw.x && e.getY() <= draw.y) {
+	    draw.moveLeft();
+	    draw.moveUp();
+	}
+	else if (e.getX() < draw.x && e.getY() > draw.y) {
+	    draw.moveLeft();
+	    draw.moveDown();
+	}
+    }
+    
+    public void mousePressed(MouseEvent e) {
+	System.out.println("mousePressed");
 	if (e.getX() >= draw.x && e.getY() <= draw.y) {
 	    draw.moveRight();
 	    draw.moveUp();
@@ -54,27 +78,27 @@ public class mainFrame extends JFrame implements MouseListener,MouseWheelListene
 	else {
 	    draw.moveUp();
 	}
-
     }
 
     public mainFrame(){
         this.draw=new mainDraw();
         addMouseListener(this);
 	addMouseWheelListener(this);
+	addMouseMotionListener(this);
     }
 
     public static void main(String[] args) {
-                mainFrame frame = new mainFrame();
-                frame.setTitle("mouse listener");
-                frame.setResizable(false);
-                frame.setSize(600, 600);
-                frame.setMinimumSize(new Dimension(600, 600));
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	mainFrame frame = new mainFrame();
+	frame.setTitle("mouse listener");
+	frame.setResizable(false);
+	frame.setSize(600, 600);
+	frame.setMinimumSize(new Dimension(600, 600));
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-                //dont understand these 2 lines
-                frame.getContentPane().add(frame.draw);
-                frame.pack();
-                frame.setVisible(true);
+	//dont understand these 2 lines
+	frame.getContentPane().add(frame.draw);
+	frame.pack();
+	frame.setVisible(true);
     }
 
 
