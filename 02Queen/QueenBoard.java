c0e80b98c8d4d352ac394c382c1f8746d088b772
rachel-ng <rng@stuy.edu>
2018-02-12 14:41:35 -0500

    uh stuff

--- 02Queen/QueenBoard.java
+++ 02Queen/QueenBoard.java
@@ -13,7 +13,12 @@ public class QueenBoard {
     private boolean addQueen(int r, int c) {
 	if (r < board.length && c < board[r].length) {
 	    if (board[r][c] == 0) {
-		board[r][c] = -1;
+		board[r][c]--;
+		for (int rr = r + 1, cc = c + 1; rr < board.length && cc < board[rr].length; rr++, cc++) {
+		    board[rr][c]++;
+		    board[r][cc]++;
+		    board[rr][cc]++;
+		}
 		return true;
 	    }
 	    else {
@@ -22,22 +27,36 @@ public class QueenBoard {
 	}
 	return false;
     }
-    /*
+    
     private boolean removeQueen(int r, int c) {
-	
+	if (r < board.length && c < board[r].length) {
+	    if (board[r][c] == -1) {
+		board[r][c]++;
+		for (int rr = r + 1, cc = c + 1; rr < board.length && cc < board[rr].length; rr++, cc++) {
+		    board[rr][c]--;
+		    board[r][cc]--;
+		    board[rr][cc]--;
+		}
+		return true;
+	    }
+	    else {
+		return false;
+	    }
+	}
+	return false;
     }
-    */
+
     public String toString() {
 	String brd= "";
 	for (int r = 0; r < board.length; r ++) {
 	    for (int c = 0; c < board[r].length; c++) {
 		if (board[r][c] == -1) {
-		    //brd += "Q ";
-		    brd += board[r][c] + " ";
+		    brd += "Q ";
+		    //brd += board[r][c] + "\t";
 		}
 		else {
 		    //brd += "_ ";
-		    brd += board[r][c] + " ";
+		    brd += board[r][c] + " "; //+ "\t";
 		}
 	    }
 	    brd += "\n";
@@ -54,9 +73,15 @@ public class QueenBoard {
     }
     */
     public static void main (String[] args) {
-	QueenBoard b = new QueenBoard(3);
+	QueenBoard b = new QueenBoard(5);
 	System.out.println(b.toString());
-	System.out.println(b.addQueen(1,1));
+	//System.out.println(b.addQueen(1,1));
+	//System.out.println(b.toString());
+	System.out.println(b.addQueen(3,2));
 	System.out.println(b.toString());
+	System.out.println(b.addQueen(1,3));
+	System.out.println(b.toString());
+	//System.out.println(b.removeQueen(1,1));
+	//System.out.println(b.toString());
     }
 }
