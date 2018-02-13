32c6273e18a1d2c91ed2f08d62fe248ed308a584
rachel-ng <rng@stuy.edu>
2018-02-13 14:14:46 -0500

    removed test cases

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -166,57 +166,11 @@ public class QueenBoard {
     }
     
     public static void main (String[] args) { // My Driver
-	QueenBoard a = new QueenBoard(10);
-	QueenBoard b = new QueenBoard(10);
+	QueenBoard b = new QueenBoard(5);
 
-	// System.out.println("B R D   A\n ");
-	// System.out.println(a.toString());
-	// System.out.println(a.addQueen(1,2));
-	// System.out.println(a.toString());
-	// System.out.println(a.removeQueen(1,2));
-	// System.out.println(a.toString());
-	
 	System.out.println(b.toString());
 	//System.out.println(b.solve());
 	System.out.println(b.toString());
-	System.out.println(b.countSolutions());
+	System.out.println("5 X 5:  " + b.countSolutions());
     }
-
-
-    /*
-    public static void main(String[] args){ // Crystal's Driver for Queen Board
-	QueenBoard b = new QueenBoard(4);
-
-	System.out.println(b.solve()); //prints true
-	System.out.println(b); //prints a valid solution
-
-	try{
-	    b.solve();
-	}catch(IllegalStateException e){
-	    System.out.println("Error: The board contains non-zero values");
-	} //prints "Error: The board contains non-zero values"
-	
-	try{
-	b.countSolutions();
-	}catch(IllegalStateException e){
-	    System.out.println("Error: The board contains non-zero values");
-	} //prints "Error: The board contains non-zero values"
-	
-	for (int i = 0; i < 12; i++){
-	    QueenBoard a = new QueenBoard(i);
-	    System.out.println("# of Solutions for " + i + ": " + a.countSolutions());
-	    /*          Expected Values
-			i --> # of Solutions   i --> # of Solutions
-			0 --> 1                      6 --> 4
-			1 --> 1                      7 --> 40
-			2 --> 0                      8 --> 92
-			3 --> 0                      9 --> 352
-			4 --> 2                    10 --> 724
-			5 --> 10                  11 --> 2680
-	    */
-    /*
-	    System.out.println(a); //prints out an empty i by i grid of underscores
-	}
-    }
-    */
 }
