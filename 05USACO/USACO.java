2fb397e85fcd8daab5a6d71ac387efaad4b8c07b
rachel-ng <rng@stuy.edu>
2018-03-06 14:42:29 -0500

    save me

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -1,3 +1,13 @@
 public class USACO {
+    public static int bronze(String filename) {
 
+    }
+
+    public static int silver(String filename) {
+
+    }
+
+    public static void main(String args[]) throws FileNotFoundException {
+
+    }
 }
