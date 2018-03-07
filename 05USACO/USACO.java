c354c065d11bd4c4bae16cef80c189c849e13138
rachel-ng <rng@stuy.edu>
2018-03-07 14:43:15 -0500

    ajkfasdf

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -1,8 +1,35 @@
-public class USACO {
-    public static int bronze(String filename) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class USACO {
+
+    private String read (String filename) {
+	File text = new File(filename);
+	Scanner inf = new Scanner(text);
+
+	String str;
+	
+	while(inf.hasNextLine()){
+            str += inf.nextLine();
+	}
+
+	return str;
+    }
+    
+    public static int bronze(String filename) {
+	return bronzeH(filename);
     }
 
+    private static int bronzeH(String filename) {
+	int vol = 0;
+	int[][];
+
+	
+	
+	return vol;
+    }
+    
     public static int silver(String filename) {
 
     }
