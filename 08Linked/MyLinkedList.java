009be9b2e7da8be33dc7e8d5f12b0e2e74d64935
rachel-ng <rng@stuy.edu>
2018-03-28 14:37:44 -0400

    akfdjklasdf

---
+++ 08Linked/MyLinkedList.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+public class MyLinkedList {
+
+    private Node first,last;
+    private int length;
+    
+    private class Node{
+	private Node next, prev;
+	private int data;
+	
+	private Node getNext () {
+	}
+	
+	private Node getPrev () {
+	}
+	
+	private int getValue () {
+	}
+	
+	private String toString (Node n) {
+	}
+	
+	private void get (Node n) {
+	    
+	}
+	
+	private void set (Node n) {
+	    
+	}
+    }
+
+    public MyLinkedList () {
+	
+    }
+
+    public String toString () {
+	String str = "[";
+
+	for () {
+	    str += 
+	}
+	
+	return str + "]";
+    }
+
+    public int get(int index) {
+	
+    }
+
+    public int set (int index, int value) {
+	
+    }
+
+    public int size () {
+	return length;
+    }
+
+    public boolean add (int index, int value) {
+	
+    }
+
+    public boolean remove (int index) {
+	if (index > length - 1) {
+	    throw new IllegalArgumentException();
+	}
+    }
+
+    
+}
