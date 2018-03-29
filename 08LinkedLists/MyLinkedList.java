c253e220216064d7b0ee83d3bcb35ce6125cf26f
rachel-ng <rng@stuy.edu>
2018-03-29 14:37:46 -0400

    not working rip

--- 08LinkedLists/MyLinkedList.java
+++ 08LinkedLists/MyLinkedList.java
@@ -2,23 +2,28 @@ import java.util.*;
 
 public class MyLinkedList {
 
-    private Node first,last;
-    private int length;
-    
+    private Node start,end;
+    private int size;
+
     private class Node{
 	private Node next, prev;
 	private int data;
 	
 	private Node getNext () {
+	    return next; 
 	}
-	
+
 	private Node getPrev () {
+	    return prev;
 	}
 	
 	private int getValue () {
+	    return data;
 	}
 	
 	private String toString (Node n) {
+	    str = "";
+	    return str;
 	}
 	
 	private void get (Node n) {
@@ -29,51 +34,86 @@ public class MyLinkedList {
 	    
 	}
     }
-
-    public MyLinkedList () {
+    
+    /*
+    private Node getNode (int index) {
 	
+    }
+    */
+    
+    public MyLinkedList () {
+	start = null;
+	end = null;
+	size = 0;
     }
 
     public String toString () {
 	String str = "[";
-
-	for () {
-	    str += 
+	
+	if (size > 0) {
+	    Node n = start;
+	    str += n;
+	    while(n != null) {
+		str += ", " + n;
+		n = n.getNext();
+	    }
 	}
 	
 	return str + "]";
     }
 
-    public int get(int index) {
-	
-    }
+    public void clear () {
 
-    public int set (int index, int value) {
-	
     }
 
     public int size () {
-	return length;
+	return size;
+    }
+    
+    public Integer get(int index) {
+	return index;
+    }
+
+    public Integer set (int index, Integer value) {
+	return value; 
     }
 
     public boolean in (int index) {
-	return index < length;
+	return index < size;
     }
 
-    public boolean add (int index, int value) {
-	if (in(index)) {
+    public boolean add (Integer newData) {
+	
+	return false; 
+    }
 
+    public void add (int index, Integer value) {
+	if (in(index)) {
 	}
 	else {
 	    throw new IllegalArgumentException();
 	}
     }
 
+    public boolean remove (Integer value) {
+	return false;
+    }
+    
     public boolean remove (int index) {
 	if (!in(index)) {
+	    return false;
+	}
+	else {
 	    throw new IllegalArgumentException();
 	}
     }
 
+    public static void main(String[]args){
+
+	MyLinkedList a = new MyLinkedList(); 
+	
+	System.out.println(a.size());
+	System.out.println(a.toString());
+    }
     
 }
