dea5f98ce314739d5e7b1f6cff2cab31e0e2c75a
rng <rng@stuy.edu>
2018-04-09 04:41:13 -0400

    I realize now that it was supposed to be first / last and not start / end mb

--- 08LinkedLists/MyLinkedList.java
+++ 08LinkedLists/MyLinkedList.java
@@ -8,42 +8,50 @@ public class MyLinkedList {
     private class Node{
 	private Node next, prev;
 	private int data;
+
+	public Node (int data) {
+	    this.data = data;
+	}
 	
 	private Node getNext () {
 	    return next; 
 	}
 
+	private Node setNext (int n) {
+	    next = n; 
+	}
+	
 	private Node getPrev () {
 	    return prev;
+	}
+
+	private Node setPrev (int n) {
+	    prev = n; 
 	}
 	
 	private int getValue () {
 	    return data;
 	}
+
+	private void setValue (int n) {
+	    data = n;
+	}
 	
 	private String toString (Node n) {
-	    str = "";
-	    return str;
+	    return "" + data;
 	}
-	
-	private void get (Node n) {
-	    
-	}
-	
-	private void set (Node n) {
-	    
-	}
-    }
-    
-    private Node getNode (int index) {
-	Node n = index;
-	return n; 
     }
     
     public MyLinkedList () {
 	start = null;
 	end = null;
 	size = 0;
+    }
+
+    public void clear () {
+	start = null;
+	end = null;
+	size = 0; 
     }
 
     public String toString () {
@@ -60,58 +68,108 @@ public class MyLinkedList {
 	
 	return str + "]";
     }
+    
+    private Node getNode (int index) {
+	if (index < 0 || index >= size) {
+	    throw new IndexOutOfBoundsException();
+	}
+	
+	Node n = start;
 
-    public void clear () {
+	for (int i = 0; i < index; i++) {
+	    n = n.getNext();
+	}
 
+	return n; 
     }
 
+    public Integer get(int index) { // exceptions
+	if (index == 0) {
+	    return start;
+	}
+	else if (index == length - 1) {
+	    return end;
+	}
+	else {
+	    return getNode(index);
+	}
+    }
+
+    public Integer set (int index, Integer value) { // exceptions
+	Node n = new Node(value);
+	return value; 
+    }
+    
     public int size () {
 	return size;
     }
-    
-    public Integer get(int index) {
-	return index;
-    }
 
-    public Integer set (int index, Integer value) {
-	return value; 
-    }
-
-    public boolean in (int index) {
+    public boolean in (int index) { // returns true if "in", returns false if not
 	return index < size;
     }
 
-    public boolean add (Integer newData) {
-	if (start == null) {
-	    return true; 
+    public boolean add (Integer value) {
+
+	Node n = new Node(value);
+	
+	if (size == 0) {
+	    first = n; 
 	}
 	else {
-	    end 
+	    n.setPrev(end);
+	    end.setNext(n);
 	}
-	return false; 
+
+	end = n;
+	size++;
+	return true;
     }
 
     public void add (int index, Integer value) {
-	if (in(index)) {
+	if (index == 0) {
+
 	}
-	else {
+	if (index == size) {
+
+	}
+	if (in(index)) {
+	    
+	}
+	else if (!in(index)) {
 	    throw new IllegalArgumentException();
 	}
     }
 
     public boolean remove (Integer value) {
+	Node n = start; 
+
+	while (n != null) {
+	    if (n.getValue() == value) {
+		Node prev = n.getPrev(); 
+		Node next = n.getNext();
+		prev.setNext(next);
+		next.setPrev(prev);
+
+		return true;
+	    }
+	    else {
+		n = n.getNext();
+	    }
+	}
+	
 	return false;
     }
     
     public boolean remove (int index) {
-	if (!in(index)) {
-	    return false;
-	}
-	else {
+	if (!in(index)) { // if not (index < size)
 	    throw new IllegalArgumentException();
 	}
-    }
+	else {
 
+	}
+	return true;
+    }
+    
     public static void main(String[]args){
 
 	MyLinkedList a = new MyLinkedList(); 
