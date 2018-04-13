c0015e7fa964c94f77c69ccd1316c39cd73db743
rng <rng@stuy.edu>
2018-04-13 00:43:59 -0400

    made file

---
+++ 08LinkedLists/Sort.java
@@ -0,0 +1,15 @@
+public class Sort{
+
+    public static void radixsort (MyLinkedListImproved<Integer> data) {
+
+    }
+
+    // probably use compareTo to find the largest value
+    // then use log to find the max # of places / digits 
+    // then use mod to isolate the last digit
+    // then use 
+
+    public static void main(String[]args) {
+
+    }
+}
