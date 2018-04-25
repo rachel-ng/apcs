be8b32fd33eb9dfc473b97eba15a477559f00c7d
rng <rng@stuy.edu>
2018-04-25 18:48:05 -0400

    rip

--- 09StackCalc/Calculator.java
+++ 09StackCalc/Calculator.java
@@ -9,44 +9,35 @@ public class Calculator {
 	String current = ""; 
 	for (int i = 0; i < s.length(); i++) {
 	    if (s.substring(i,i+1).equals("+") || s.substring(i,i+1).equals("-") || s.substring(i,i+1).equals("*") || s.substring(i,i+1).equals("/") || s.substring(i,i+1).equals("%")){
-		current = s.substring(i,i+1);
-		current = "" + process(current, Double.parseDouble(data.pop()), Double.parseDouble(data.pop()));
+		current = "" + process(s.substring(i,i+1), Double.parseDouble(data.pop()), Double.parseDouble(data.pop()));
 		data.push(current);
-		System.out.println(current + " push");
 	    }
-	    if (s.substring(i,i+1).equals(" ")) {
+	    else if (s.substring(i,i+1).equals(" ")) {
 		data.push(current);
-		System.out.println(current + " push");
 		current = "";
 	    }
 	    else if (!(s.substring(i,i+1).equals("+") || s.substring(i,i+1).equals("-") || s.substring(i,i+1).equals("*") || s.substring(i,i+1).equals("/") || s.substring(i,i+1).equals("%") || s.substring(i,i+1).equals(" "))){
 		current += s.substring(i,i+1);
 	    }
 	}
-	return current;
+	return data.pop();
 	
     }
 
     public static double process (String operator, double b, double a) {
-	System.out.println(a + " " + operator + " " +  b);
 	if (operator.equals("+")) {
-	    System.out.println(a + b);
 	    return a + b;
 	}
 	else if (operator.equals("-")) {
-	    System.out.println(a - b);
 	    return a - b;
 	}
 	else if (operator.equals("*")){
-	    System.out.println(a * b);
 	    return a * b;
 	}
 	else if (operator.equals("/")) {
-	    System.out.println(a / b);
 	    return a / b;
 	}
 	else {
-	    System.out.println(a % b);
 	    return a % b;
 	}
     }
@@ -55,6 +46,7 @@ public class Calculator {
 	System.out.println(eval("10 2.0 +") + "\n");
 	System.out.println(eval("11 3 - 4 + 2.5 *") + "\n");
 	System.out.println(eval("8 2 + 99 9 - * 2 + 9 -") + "\n");
+	System.out.println(eval("8 2 + 99 9 - *") + "\n");
 	System.out.println(eval("3 2 -") + "\n");
     }
     
