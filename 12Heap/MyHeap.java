f9f654eee4b41cf4322604aee43e7c3169d41642
rng <rng@stuy.edu>
2018-05-02 01:23:26 -0400

    halp

--- 12Heap/MyHeap.java
+++ 12Heap/MyHeap.java
@@ -23,35 +23,68 @@ public class MyHeap{
     // Methods
 
     public String toString (){
-	if(this==null){
-	    return"[]";
-	}else{
-	    return Arrays.toString(arr);
+	String str = "[";
+	for (int i = 0; i < size; i++) {
+	    str += data[i];
+	    if (i < size - 1) {
+		str += ", ";
+	    }
 	}
+	return str + "]"
     }
 
     public void add (String s) {
-	if (data[0] == null) {
-	    data[0] = s;
+	if (size == data.length) {
+	    resize();
+	}
+
+	data[size] = s;
+	up(size);
+	size++;
+    }
+
+    public void up (int index) {
+	int p = (index - 1) /2;
+
+	if ( ) {
+
 	}
     }
 
-    public String remove () {
+    public void down (int index) {
+	int l = index * 2 + 1;
+	int r = index * 2 + 2;
 	
+    }
+
+    public String remove () {
+	String s = peek();
+	swap(0, size - 1);
+	length--;	
+	pushDown(0);	
+	return s;
     }
     
     public String peek () {
-	
+	return data[0];
     }
 
     public int size () {
 	return size;
     }
 
-    public static void swap (int a, int b, String[] data) {
+    public void swap (int a, int b, String[] data) {
 	String c = data[a];
 	data[a] = data[b];
 	data[b] = c;
+    }
+
+    public void resize () {
+	String[] str = new String[size * 2];
+	for (int i = 0; i < size; i++) {
+	    str[i] = data[i];
+	}
+	data = str;
     }
     
 }
--- 13Medians/RunningMedians.java
+++ 13Medians/RunningMedians.java
@@ -2,4 +2,10 @@ import java.util.*;
 
 public class RunningMedians {
 
+    public RunningMedians {
+
+    }
+
+    // min tree and max tree
+    
 }
