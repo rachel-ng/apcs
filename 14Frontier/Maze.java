65ebe448c004657678da3906d655325009cadd30
rng <rng@stuy.edu>
2018-05-09 01:55:41 -0400

    asdlkfj

--- 14Frontier/Maze.java
+++ 14Frontier/Maze.java
@@ -1,48 +1,39 @@
 public class Maze{
-    private Location start,end;
-    private char[][] board;
+  private Location start,end;
+  private char[][] board;
 
-    public Maze(String mazeText){
-	for (int i = 0; i < mazeText.length(); i++) {
+  public Maze(String mazeText){
+  }
 
-	}
-    }
-
-    // '#' - wall 
-    // ' ' - open space
-    // '?' - frontier space
-    // '.' - visited space
-    // 'E' - end space (do not replace this)
-    // if (maze[r][c] == 'E') {
-    //     end = Location (r, c);
-    // }
-    // '@' - part of solution
-    // 'S' - starting space (do not replace this)
-    // if (maze[r][c] == 'S') {
-    //     start = Location (r, c);
-    // }
-    public String toString(){
-	
-    }
+  // '#' - wall 
+  // ' ' - open space
+  // '?' - frontier space
+  // '.' - visited space
+  // 'E' - end space (do not replace this)
+  // '@' - part of solution
+  // 'S' - starting space (do not replace this)
+  public String toString(){
+  }
    
-    // Work on this method as a group! 
-    public String toStringColor(){
-	
-    }
+  // Work on this method as a group! 
+  public String toStringColor(){
+  }
 
-    //return a list of Locations that are:
-    // adjacent to n and  not visited
-    // all the Locations in this list should have their previous set to n.
-    public Location[] getNeighbors(Location n){
-	return null;
-    }
+  //return a list of Locations that are:
+  // adjacent to n and  not visited
+  // all the Locations in this list should have their previous set to n.
+  public Location[] getNeighbors(Location n){
+    return null;
+  }
 
-    public Location getStart(){
-	return null;
-    }
+  public Location getStart(){
+    return null;
+  }
 
-    public Location getEnd(){
-	return null;
-    }
+  public Location getEnd(){
+    return null;
+  }
+
+
 
 }
