a08a5b33a3ba876a0dd79d5f79ff84116ef3a3b2
rng <rng@stuy.edu>
2018-05-10 03:37:45 -0400

    aljfd

--- 13Medians/RunningMedian.java
+++ 13Medians/RunningMedian.java
@@ -2,33 +2,43 @@ import java.util.*;
 
 public class RunningMedian {
 
-    private int size;
     private MyHeap<Double> smol, tol;
     
     public RunningMedian() {
 	smol = new MyHeap<Double>();
 	tol = new MyHeap<Double>(false);
-	size = 0;
     }
-    // - makes an empty container for Doubles.
     
     public void add(Double value) {
-	if (value > getMedian()) {
+	if (smol.size() == 0 || value < smol.peek()){
+	    smol.add(value);
+	}
+	else {
 	    tol.add(value);
 	}
-	else if (value < getMedian() {
-		smol.add(value);
-	    }
+
+	if (smol.size() - tol.size() > 1) {
+	    tol.add(smol.remove());
+	}
+	else if (tol.size() - smol.size() > 1) {
+	    smol.add(tol.remove());
+	}
+	
     }
-    // - insert a Double into the data structure.
     
     public Double getMedian() {
-
-    }
-    // - return the current median, throws NoSuchElementException when size is 0.
-    
-    public int size() {
-	return size;
+	if (smol.size() == 0 || tol.size() == 0) {
+	    throw new NoSuchElementException();
+	}
+	
+	if (smol.size() > tol.size()) {
+	    return smol.peek();
+	}
+	else if (tol.size() > smol.size()) {
+	    return tol.peek();
+	}
+	
+	return (smol.peek() + tol.peek()) / 2;
     }
 
     public MyHeap getSmol () {
@@ -37,6 +47,22 @@ public class RunningMedian {
 
     public MyHeap getTol () {
 	return tol;
+    }
+
+    public static void main(String[]args){
+	
+	RunningMedian x = new RunningMedian();
+
+	x.add(1.0);
+	x.add(3.0);
+	x.add(5.0);
+	x.add(7.0);
+	x.add(9.0);
+	x.add(11.0);
+	System.out.println("s: " + x.getSmol());
+	System.out.println("l: " + x.getTol());
+	System.out.println("median: " + x.getMedian());
+	
     }
     
 }
