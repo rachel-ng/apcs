a188c4097272c74ebf4d6da2ce6be890be8ca616
rng <rng@stuy.edu>
2018-06-08 04:20:37 -0400

    without the driver

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,78 +1,85 @@
-public  class Merge {
+import java.util.*;
+
+public class Merge {
+
+    public static void merge (int[] data, int[] temp, int start, int humpdayyyyy, int end) { // aka the squish function 
+	int i = start;
+	int ii = humpdayyyyy + 1;
+	int dataIndex = start;
+	while (i <= humpdayyyyy || ii <= end) {
+	    if (i > humpdayyyyy) {
+		data[dataIndex] = temp[ii];
+		ii++;
+	    }
+	    else if (ii > end) {
+		data[dataIndex] = temp[i];
+		i++;
+	    }
+	    else if (temp[i] <= temp[ii]) {
+		data[dataIndex] = temp[i];
+		i++;
+	    }
+	    else {
+		data[dataIndex] = temp[ii];
+		ii++;
+	    }
+	    dataIndex++;
+	}
+    }
+
+    public static void mergesort (int[] data) {
+	int[] temp = new int[data.length];
+	msort2(data, temp, 0, data.length - 1);
+    }
+
+    private static void msort (int[] data, int[] temp, int lo, int hi) {
+	if (lo >= hi) {
+	    return; // to sender
+	}
+	for (int i = lo; i < hi + 1; i++) {
+	    temp[i] = data[i];
+	}
+	int humpdayyyyy = (lo / 2) + (hi / 2);
+	msort(temp, data, lo, humpdayyyyy);
+	msort(temp, data, humpdayyyyy + 1, hi);
+	merge(data, temp, lo, humpdayyyyy, hi);
+    }
+
+    private static void msort2 (int[] data, int[] temp, int lo, int hi) {
+	if (hi - lo <= 7) {
+	    insertionSort(data, lo, hi);
+	    return;
+	}
+	for (int i = lo; i < hi + 1; i++) {
+	    temp[i] = data[i];
+	}
+	int humpdayyyyy = (lo/2) + (hi/2);
+	msort(temp, data, lo, humpdayyyyy);
+	msort(temp, data, humpdayyyyy + 1, hi);
+	merge(data, temp, lo, humpdayyyyy, hi);
+    }
 
     public static void swap (int[] data, int a, int b) {
 	int c = data[a];
 	data[a] = data[b];
 	data[b] = c;
     }
-    
-    public static int[] merge (int[] data, int[] temp, int lo, int hi) {
-	int mid = (lo + hi) / 2;
-	int midd = ((lo + hi) / 2) + 1; 
-		
-	for (int i = lo; i < hi + 1; i++) {
-	    if (lo <= mid) {
-		if (midd <= hi) {
-		    if (data[lo] <= data[midd]) {
-			temp[i] = data[lo];
-			lo++;
-		    }
-		    else if (data[midd] < data[lo]){
-			temp[i] = data[midd];
-			midd++;
-		    }
-		}
-		else {
-		    temp[i] = data[lo];
-		    lo++;
-		}
-	    }
-	    else if (midd <= hi){
-		temp[i] = data[midd];
-		midd++;
-	    }
-	}
 
-	return temp; 
-    }
-
-    public static void mergesort(int[]data) {
-	int[] temp = new int[data.length];
-	msort(data, temp, 0, data.length - 1);
-    }
-
-    private static void msort(int[]data, int[]temp, int lo, int hi) {
-	int mid = (lo + hi) / 2;
-	if (lo >= hi) {
-	    return;
-	}
+    public static void insertionSort (int[] data, int start, int end) {
+	int i = start;
 	
-	for (int i = lo; i <= hi + 1; i++) {
-	    temp[i] = data[i];
+	while (i < end) {
+	    int j = i;
+	    while (j > 0 && data[j - 1] > data[j]) {
+		swap(data, j, j - 1);
+		j = j - 1;
+	    }
+	    i++;
 	}
-
-	msort(temp, data, lo, mid);
-	msort(temp, data, mid + 1, hi);
-	merge(data,temp,lo,hi);
-	
-    }
-
-    public static String toString(int[] ary) { // for printing out arrays
-	String str = "{" + ary[0];
-	for (int i = 1; i < ary.length; i++) {
-	    str += ", " + ary[i];
-	}
-	return str + "}";
     }
 
     public static void main(String[]args){
-	int[] data = {1, 3, 4, 7, 8, 0, 3, 5, 6, 9};
-	int[] dataa = new int[10];
 
-	System.out.println(toString(data));
-	System.out.println(toString(dataa));
-	merge(data, dataa, 0, 9);
-	System.out.println(toString(data));
-	System.out.println(toString(dataa));
     }
+
 }
