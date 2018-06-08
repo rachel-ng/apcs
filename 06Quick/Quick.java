4f68078405d91cce64ee1629446917af5c9859bc
rng <rng@stuy.edu>
2018-06-08 04:20:07 -0400

    like a self driving car

--- 06Quick/Quick.java
+++ 06Quick/Quick.java
@@ -1,6 +1,6 @@
-zimport java.util.Random;
+import java.util.*;
 
-public class Quick{
+public class Quick {
 
     public static void swap (int[] data, int a, int b) {
 	int c = data[a];
@@ -8,141 +8,123 @@ public class Quick{
 	data[b] = c;
     }
 
-    public static int partition (int[] ary, int start, int end) {
-	Random rng = new Random();
-	int r = rng.nextInt(end - start + 1) + start;
+    public static int partition ( int[] data, int start, int end) {
+	int n = (int)(Math.random() * (end - start) + start);
+	int pivot = data[n];
 	
-	int pivot = ary[r];
-	swap(ary, r, start);
-
 	int s = start + 1;
 	int e = end;
 	
+	swap(data, n, start);
 	while (s <= e) {
-	    if (ary[s] < pivot) {
+	    if (data[s] <= pivot) {
 		s++;
 	    }
 	    else {
-		swap(ary, s, e);
+		swap(data, s, e);
 		e--;
 	    }
 	}
-	swap(ary, start, e);
+	swap(data, start, e);
 	return e;
     }
 
-    public static int quicksort (int[] ary) {
-	int lo = 0;
-	int hi = ary.length - 1;
+    public static int quickselect (int[] data, int k) {
+	int start = 0;
+	int end = data.length -1;
 
-	Random rng = new Random();
-	int pi = rng.nextInt(hi - lo + 1) + lo;
-	
-	swap(ary, pi, lo);
-	
-	int i = lo + 1;
-	int j = hi;
-
-	while (i <= j) {
-	    if (ary[i] >= ary[lo]) {
-		swap(ary, i, j);
-		j--;
+	for (int i = 0; i < data.length; i++) {
+	    int[] output = dutchPartition(data, start, end);
+	    int hi = output[1];
+	    int lo = output[0];
+	    
+	    if (k >= lo && k <= hi) {
+		return data[k];
+	    }
+	    else if (k > hi) {
+		start = hi + 1;
 	    }
 	    else {
-		i++;
+		end = lo - 1;
 	    }
 	}
-	swap(ary, j, lo);
-	return j;
+	return 0;
     }
 
-    public static int quickselect(int[] ary, int k){
-	int s = 0;
-	int e = ary.length - 1;
+    public static void quicksort (int[] data) {
+	quicksortH2(data, 0, data.length-1);
+    }
 
-	for (int i = 0; i < ary.length; i++) {
-	    int in = partition(ary, s, e);
-	    if (in == k) {
-		return ary[in];
+    private static void quicksortH (int[] data, int start, int end) {
+	int[] output = dutchPartition(data, start, end);
+	if (output[0] != start) {
+	    quicksortH (data, start, output[0] - 1);
+	}
+	if (output[1] != end) {
+	    quicksortH (data, output[1] + 1, end);
+	}
+    }
+
+    private static void quicksortH2 (int[] data, int start, int end) {
+	if (end - start <= 7) {
+	    insertionSort(data, start, end);
+	    return;
+	}
+	int[] output = dutchPartition(data, start, end);
+	if (output[0] != start) {
+	    quicksortH (data, start, output[0] - 1);
+	}
+	if (output[1] != end) {
+	    quicksortH (data, output[1] + 1, end);
+	}
+    }
+
+    public static int[] dutchPartition(int[] data, int start, int end) {
+	int n = (int)(Math.random() * (end - start) + start);
+	int pi = data[n];
+	
+	int s = start;
+	int e = end;
+	int at = start;
+	
+	while (at <= e) {
+	    if (data[at] == pi) {
+		at++;
 	    }
-	    else if (in < k) {
-		s = in + 1;
+	    else if (data[at] > pi) {
+		swap(data, at, e);
+		e--;
 	    }
 	    else {
-		e = in - 1;
+		swap(data, s, at);
+		s++;
+		at++;
 	    }
 	}
-	return -1; 
+	
+	int[] output = new int[2];
+	
+	output[0] = s;
+	output[1] = e;
+	
+	return output;
     }
 
-    public static int dutch(int[] ary){
-	int lo = 0;	
-	int hi = ary.length - 1;
+    public static void insertionSort (int[] data, int start, int end) {
+	int i = start;
 	
-	int lt = 0; 
-	int gt = ary.length - 1;
+	while (i < end) {
+	    int j = i;
+	    while (j > 0 && data[j - 1] > data[j]) {
+		swap(data, j, j - 1);
+		j = j - 1;
+	    }
+	    i++;
+	}
+    }
 
-	for (int i = 0; i < hi + 1; i++) {
-	    int in = partition(ary, lt, gt);
-	    if (in == ee) {
-		return ary[in];
-	    }
-	    else if (in < ee) {
-		lo = in + 1;
-	    }
-	    else {
-		hi = in - 1;
-	    }
-	}
-	return -1; 
-    }
-    
-    public static String toString(int[] ary) { // for printing out arrays
-	String str = "{" + ary[0];
-	for (int i = 1; i < ary.length; i++) {
-	    str += ", " + ary[i];
-	}
-	return str + "}";
-    }
-	
-    
     public static void main(String[]args){
 
-	//int[] data = {17, 61, 67, 47, 93, 12, 20, 4, 44, 68}; // array from partition
-	//int[] data = {2, 10, 15, 23, 0, 5}; // array from quickselect
-	//int[] data = {999, 999, 999, 4, 1, 0, 3, 2, 999, 999, 999}; // array from quickselect
-	int[] data = {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 2, 0, 3, 0, 1, 4, 0}; // array from dutch flag
-	
-	/* for partition
-	System.out.println(toString(data));
-	System.out.println(partition(data, 0, 9));
-	System.out.println(toString(data));
-	*/
-
-
-	/* for quicksort
-	System.out.println(toString(data1));
-	System.out.println(quicksort(data1));
-	System.out.println(toString(data1));
-	*/
-	
-	/*  for quickselect
-	System.out.println(toString(data));
-	System.out.println(quickselect(data,0)); // 0 / 0
-	System.out.println(quickselect(data,1)); // 2 / 1
-	System.out.println(quickselect(data,3)); // 5 / 2
-	System.out.println(quickselect(data,5)); // 10 / 3
-	System.out.println(quickselect(data,9)); // 15 / 4
-	System.out.println(quickselect(data,16)); // 23 / 999
-	System.out.println(toString(data));
-	*/
-
-	/*  for dutch */
-	System.out.println(toString(data));
-	System.out.println(dutch(data));
-	System.out.println(toString(data));
-	/**/
-
-	
     }
+  
 }
